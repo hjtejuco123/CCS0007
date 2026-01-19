5e572c02b37a5f02bd4ec211be5e76425a0b18d5
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-01-19 15:15:36 +0800

    Create array5.cpp

---
+++ Module2/01192026/array5.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+Two dim array 
+    data_type arrayname [row][col];
+    int arrnum[10][10]; 
+    
+    3 x 3
+    1,1     1,2     1,3
+    1       2       3 
+    2,1     2,2     2,3 
+    4       5       6 
+    3,1     3,2     3,3 
+    7       8       9
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m, i,j, a[10][10],t=0;
+    cout << "Enter order of matrix " << endl;
+    cin >> n >> m;
+    for (i=1;i<=n;i++)
+        for (j=1; j<=m;j++)
+            cin >> a[i][j];
+            
+    cout << "given matrix is \n";
+    for (i=1; i<=n;i++){
+        for (j=1; j<=m;j++)
+            cout << "\t" << a[i][j];
+        cout << endl;
+    }
+    
+    if (m==n){
+        for (i=1; i<=n;i++)
+            for (j=1;j<=m;j++)
+                if (i==j)
+                    t+=a[i][j];
+        cout << "value is " << t;
+    }else{
+        cout << "Not a trace matrix "<<endl;
+        
+    }
+    return 0;
+}
+
+
