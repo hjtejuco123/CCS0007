eaff2093e87a3a53c7a2be0edca9c88b4b88145d
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-01-27 10:46:09 +0800

    Create sample4.cpp

---
+++ Module 3/sample4.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+//put
+*******************************************************************************/
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    cout.put('A');
+    cout.put('p');
+    cout.put('p');
+    cout.put('l');
+    cout.put('e');
+    return 0;
+}
