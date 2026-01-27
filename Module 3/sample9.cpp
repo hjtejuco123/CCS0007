be8b6d65c7470d85dc9d0f63b6b99c3eef6114de
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-01-27 11:41:35 +0800

    Create sample9.cpp

---
+++ Module 3/sample9.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+.length
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str1 = "apple";
+    string str2 = "apple";
+    if (str1==str2)
+        cout << "Equaal ";
+    else 
+        cout << "Not equal ";
+
+    return 0;
+}
